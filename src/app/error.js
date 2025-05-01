62bc866afac84195fc412885d7cdf765ec55d393
Insy Hernsvan <insy.hernsvan@envisionpharma.com>
2025-05-01 13:18:07 -0400

    Fix error.js

--- src/app/error.js
+++ src/app/error.js
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect } from 'react';
-export default function error({ error, reset }) {
+export default function ErrorPage({ error, reset }) {
   useEffect(() => {
     console.log(error);
   }, [error]);
